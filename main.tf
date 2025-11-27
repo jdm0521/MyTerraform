135292e3fcc753a1b6f0cd707f2bc8cde82d42a8
jdm0521 <39712829+jdm0521@users.noreply.github.com>
2025-11-27 14:36:26 -0600

    Changed the private IP allocation to Static

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "azurerm_network_interface" "jdm-nic" {
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.jdm-subnet.id
-    private_ip_address_allocation = "Dynamic"
+    private_ip_address_allocation = "Static"
     public_ip_address_id          = azurerm_public_ip.jdm-ip.id
   }
 
