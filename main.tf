f4963c709b1c5218ab455d70337fec00018f6f59
jdm0521 <39712829+jdm0521@users.noreply.github.com>
2025-11-28 18:55:20 -0600

    Changing the ip to dynamic for nic card

--- main.tf
+++ main.tf
@@ -91,7 +91,7 @@ resource "azurerm_network_interface" "jdm-nic" {
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.jdm-subnet.id
-    private_ip_address_allocation = "Static"
+    private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.jdm-ip.id
   }
 
